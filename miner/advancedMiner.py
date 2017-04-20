99dca5135b0510eb89103ed281995a0f6a0c9d5e
Christian Cygnus <gamer00@att.net>
2017-04-20 10:52:38 -0500

    Edit to change "ret" and to follow pep8

--- miner/advancedMiner.py
+++ miner/advancedMiner.py
@@ -7,18 +7,18 @@ import RAKE.rake as rk
 
 
 def import_text(file_path):
-	file = open(file_path, 'r')
-	data = file.read().replace('\n', ' ')
-	return data
+    file = open(file_path, 'r')
+    data = file.read().replace('\n', ' ')
+    return data
 
 
 def textToFile(text):
-	inputFiles = ["input.txt", "../input.txt"]
-	for fileName in inputFiles:
-		file = open(fileName, "w")
-		text = text.replace('\n', '').replace('\r', '')
-		file.write(text)
-		file.close()
+    inputFiles = ["input.txt", "../input.txt"]
+    for fileName in inputFiles:
+        file = open(fileName, "w")
+        text = text.replace('\n', '').replace('\r', '')
+        file.write(text)
+        file.close()
 
 
 def process_text(text):
@@ -26,16 +26,16 @@ def process_text(text):
     keywords = find_keywords(text)
     summary, uniques = calculate_MI(keywords)
     sentenceDict = map_keyphrase_sentence(uniques, text)
-    return {"summary":summary, "sentences":sentenceDict}
-
+    return {"summary": summary, "sentences": sentenceDict}
 
 
 def callMIScript(phrase1, phrase2):
-	proc = subprocess.Popen(["./js/calcMI.js", "--phrase1=" +
-							phrase1, "--phrase2=" + phrase2],
-							stdout=subprocess.PIPE)
-	line = proc.stdout.readline()
-	return line
+    proc = subprocess.Popen(["./js/calcMI.js",
+                             "--phrase1=" + phrase1,
+                             "--phrase2=" + phrase2],
+                            stdout=subprocess.PIPE)
+    line = proc.stdout.readline()
+    return line
 
 
 def calculate_MI(keywords):
@@ -50,7 +50,7 @@ def calculate_MI(keywords):
         phrase1 = keywords[i][0].encode("ascii", "ignore").decode("ascii")
         if phrase1 != keywords[i][0]:
             continue
-        subPhrases=[]
+        subPhrases = []
         if phrase1 not in uniqueKeyphrases:
             uniqueKeyphrases.add(phrase1)
         for j in range(i, len(keywords)):
@@ -66,7 +66,8 @@ def calculate_MI(keywords):
             subPhrases.append((phrase2, mi))
             # keywordMI[(phrase1, phrase2)] = mi
         # print()
-        subPhrases = [tup[0] for tup in sorted(subPhrases, key = lambda tup: tup[1])[::-1][:4]]
+        subPhrases = [tup[0] for tup in sorted(
+            subPhrases, key=lambda tup: tup[1])[::-1][:4]]
         summaryLine = phrase1 + ': ' + ', '.join(subPhrases)
         for elem in subPhrases:
             if elem not in uniqueKeyphrases:
@@ -80,64 +81,63 @@ def calculate_MI(keywords):
 
 
 def find_keywords(text):
-	"""
-	This function finds all the keywords.
-	
-	Args:
-		text (string): Where we extract the keywords from
-	
-	Returns:
-		keywords (list): A list of tuples that contains the key words
-	"""
-	keywords = rk.Rake("SmartStoplist.txt")
-	return keywords.run(text)
+    """
+    This function finds all the keywords.
+
+    Args:
+            text (string): Where we extract the keywords from
+
+    Returns:
+            keywords (list): A list of tuples that contains the key words
+    """
+    keywords = rk.Rake("SmartStoplist.txt")
+    return keywords.run(text)
 
 
 def map_keyphrase_sentence(key_phrase, text):
-	"""
-	This function maps the key phrases into each sentence the
-	key phrases are present in.
-	
-	Args:
-		key_phrase (list): List of key phrases
-		text (string): Big body of input text
-	
-	Returns:
-		ret (dict): A mapping of the key phrases to each sentence it is in
-	"""
+    """
+    This function maps the key phrases into each sentence the
+    key phrases are present in.
 
-	ret = dict()
-	# we split the blob of text into sentences
-	split_text = sent_tokenize(text)
-	for sentence in split_text:
-		for phrase in key_phrase:
-			# checks to see if a phrase is in a sentence
-			if(phrase in sentence):
-				ret = add_to_dict(ret, phrase, sentence)
-	return ret
+    Args:
+            key_phrase (list): List of key phrases
+            text (string): Big body of input text
+
+    Returns:
+            ret (dict): A mapping of the key phrases to each sentence it is in
+    """
+
+    ret = dict()
+    # we split the blob of text into sentences
+    split_text = sent_tokenize(text)
+    for sentence in split_text:
+        for phrase in key_phrase:
+            # checks to see if a phrase is in a sentence
+            if(phrase in sentence):
+                ret = add_to_dict(ret, phrase, sentence)
+    return ret
 
 
 def add_to_dict(dictionary, key, val):
-	"""
-	This function adds an item to a dictionary. Just some helper code.
-	
-	Args:
-		dictionary (dict): The dictionary we want to add an item to
-		key (string): The key of the dictionary
-		val (val): The value we want to add into the dictionary
-	
-	Returns:
-		dictionary (dict): The dictionary with the added value
-	"""
-	if(key in dictionary):
-		arr = dictionary.get(key)
-		arr.append(val)
-		ret[key] = arr
-	else:
-		arr = [val]
-		ret[key] = arr
-	return dictionary
+    """
+    This function adds an item to a dictionary. Just some helper code.
 
+    Args:
+            dictionary (dict): The dictionary we want to add an item to
+            key (string): The key of the dictionary
+            val (val): The value we want to add into the dictionary
+
+    Returns:
+            dictionary (dict): The dictionary with the added value
+    """
+    if(key in dictionary):
+        arr = dictionary.get(key)
+        arr.append(val)
+        dictionary[key] = arr
+    else:
+        arr = [val]
+        dictionary[key] = arr
+    return dictionary
 
 
 # calculate_MI(find_keywords(import_text("input.txt")))
@@ -146,17 +146,17 @@ def add_to_dict(dictionary, key, val):
 
 
 # REFERENCE MATERIAL
-	# tokenizer = nl.tokenize.RegexpTokenizer(r'\w+')
-	# tokens = tokenizer.tokenize(text)
-	# stopWords = set(stopwords.words('english'))
-	# words = []
-	# for w in tokens:
-	#     if w not in stopWords:
-	#         words.append(w)
+    # tokenizer = nl.tokenize.RegexpTokenizer(r'\w+')
+    # tokens = tokenizer.tokenize(text)
+    # stopWords = set(stopwords.words('english'))
+    # words = []
+    # for w in tokens:
+    #     if w not in stopWords:
+    #         words.append(w)
 
-	# bigrams = nl.ngrams(words, 2)
-	# trigrams = nl.ngrams(words, 3)
+    # bigrams = nl.ngrams(words, 2)
+    # trigrams = nl.ngrams(words, 3)
 
-	# print(words)
-	# print(Counter(bigrams))
-	# print(Counter(trigrams))
+    # print(words)
+    # print(Counter(bigrams))
+    # print(Counter(trigrams))
